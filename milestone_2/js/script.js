5508ffcde178d28e6e87bf93060e872b897b276a
NicoloYan <nico.yan@outlook.it>
2022-06-07 16:25:19 +0200

    Milestone 2 complete

--- milestone_2/js/script.js
+++ milestone_2/js/script.js
@@ -1,17 +1,27 @@
 
+const listElement = document.getElementById('list');
+
 for (let i = 1; i <= 100; i++) {
     let fizzBuzzOrFizzbuzz;
 
+    let li = document.createElement('li');
+
     if (i % 3 === 0 && i % 5 === 0) {
         fizzBuzzOrFizzbuzz = 'FizzBuzz';
+        li.innerHTML = fizzBuzzOrFizzbuzz
     } else if (i % 3 === 0) {
         fizzBuzzOrFizzbuzz = 'Fizz';
+        li.innerHTML = fizzBuzzOrFizzbuzz
     } else if (i % 5 === 0) {
         fizzBuzzOrFizzbuzz = 'Buzz';
+        li.innerHTML = fizzBuzzOrFizzbuzz
     } else {
         fizzBuzzOrFizzbuzz = i
+        li.innerHTML = fizzBuzzOrFizzbuzz
 
     }
 
-    console.log(fizzBuzzOrFizzbuzz);
+    listElement.append(li)
 }
+
+
